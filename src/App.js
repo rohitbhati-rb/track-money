17f1c9910a9d3af9662f911bb9186b19d537aab9
Rohit Bhati <rohitbhati8833@gmail.com>
2022-08-04 14:50:42 +0530

    added routes and some file changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import useMediaQuery from '@mui/material/useMediaQuery';
 import "@fontsource/roboto";
 import Navbar from "./components/Navbar";
-import SimpleBottomNavigation from "./components/BottomNavigation";
+import BottomNav from "./components/BottomNav";
+import AllTransactions from "./components/AllTransactions";
+import ManageAccounts from "./components/ManageAccounts";
+import Reports from "./components/Reports";
 
 const light = {
   palette: {
@@ -20,15 +24,20 @@ const dark = {
 
 const App = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(true);
-  const changeTheme = () => {
-    setIsDarkTheme(!isDarkTheme);
-  };
+  const changeTheme = () => { setIsDarkTheme(!isDarkTheme); };
+
   const smallScreen = useMediaQuery('(max-width:900px)');
+
   return (
     <ThemeProvider theme={isDarkTheme ? createTheme(dark) : createTheme(light)}>
       <CssBaseline />
       <Navbar isDarkTheme={isDarkTheme} changeTheme={changeTheme} />
-      {smallScreen ? <SimpleBottomNavigation /> : ""}
+      <Routes>
+        <Route path="/" element={<AllTransactions />} />
+        <Route path="/accounts" element={<ManageAccounts />} />
+        <Route path="/reports" element={<Reports />} />
+      </Routes>
+      {smallScreen ? <BottomNav /> : ""}
     </ThemeProvider>
   );
 }
