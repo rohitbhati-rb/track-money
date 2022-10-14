34e50e18d415889f9cd1d5af53a85590b115f0f7
Rohit Bhati <rohitbhati8833@gmail.com>
2022-10-15 00:32:34 +0530

    added basic layout for txns

--- src/components/AllTransactions.jsx
+++ src/components/AllTransactions.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState } from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Typography
+} from '@mui/material';
+import { v4 as uuidv4 } from 'uuid';
 
-function AllTransactions() {
+const allTransactions = () => {
   return (
-    <div>All Transactions</div>
+    <Container maxWidth="xl" sx={{ marginTop: 2 }}>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography variant="h5" gutterBottom component="div" sx={{ marginBottom: 2 }}>
+          My Transactions
+        </Typography>
+        <Button
+          size='small'
+          variant='contained'
+          sx={{ textTransform: "none", background: "orange", fontSize: 18 }}
+        // onClick={}
+        >
+          Add Transaction
+        </Button>
+      </Box>
+      <Box sx={{ height: "100%", width: "100%" }}>
+
+      </Box>
+    </Container>
   )
 }
 
-export default AllTransactions;
+export default allTransactions;
